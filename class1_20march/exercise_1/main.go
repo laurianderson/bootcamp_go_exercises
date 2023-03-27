d7c9e50d3226092315c7807d82115811b45e97c0
laurianderson <laura.anderson@mercadolibre.com>
2023-03-27 17:12:59 -0300

    feat: add folder class1_20march with folder exercise_1 and file main.go

---
+++ class1_20march/exercise_1/main.go
@@ -0,0 +1,9 @@
+/* Imprimir por consola Hola Mundo! */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/laurianderson/bootcamp_go_exercises/class1_20march
+
+go 1.20
