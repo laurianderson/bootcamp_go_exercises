baa83a8e24ce3ba89c761550590db798a9ab1c19
laurianderson <laura.anderson@mercadolibre.com>
2023-03-27 17:16:32 -0300

    feat: add folder exercise_2 and file main.go

---
+++ class1_20march/exercise_2/main.go
@@ -0,0 +1,9 @@
+/* Imprimir por pantalla: nombre y dirección de la persona */
+package main
+
+import "fmt"
+
+func main() {
+	nombre, direccion := "Laura Anderson", "Calle Siempreviva 123" 
+	fmt.Println("Nombre:" + nombre,  "Dirección:" + direccion)
+}
