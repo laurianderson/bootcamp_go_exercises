35e6b54580ed409612a796a738d27741043de111
laurianderson <laura.anderson@mercadolibre.com>
2023-03-27 17:35:40 -0300

    feat: add folder class2_27march with folder exercise_1 and file main.go

---
+++ class2_27march/exercise_1/main.go
@@ -0,0 +1,25 @@
+/*
+Una empresa de chocolates necesita calcular el impuesto de sus empleados al momento de depositar el sueldo, para cumplir el objetivo es necesario crear una 
+función que devuelva el impuesto de un salario. 
+Teniendo en cuenta que si la persona gana más de $50.000 se le descontará un 17 % del sueldo y si gana más de $150.000 se le descontará además un 10 % (27% en total).
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(impuestoSalario(150000.00))
+}
+
+func impuestoSalario(sueldo float64) float64 {
+	if sueldo > 50000.00  && sueldo <= 150000.00 {
+		return sueldo * 0.17
+	}else if sueldo > 150000.00 {
+		return sueldo * 0.27
+	}else {
+		return 0
+	}
+}
