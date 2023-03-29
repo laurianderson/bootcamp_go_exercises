7a9a82acece34fcbe69d4ffde09a6e2fa2087538
laurianderson <laura.anderson@mercadolibre.com>
2023-03-29 13:07:30 -0300

    update: use method Error to resolve

--- class4_29march/exercise_1/main.go
+++ class4_29march/exercise_1/main.go
@@ -1,7 +1,7 @@
 /*
 En tu función “main”, definí una variable llamada “salary” y asignale un valor de tipo “int”.
-Creá un error personalizado con un struct que implemente “Error()” con el mensaje “Error: el salario 
-ingresado no alcanza el mínimo imponible" y lanzalo en caso de que “salary” sea menor a 150.000. De lo contrario, 
+Creá un error personalizado con un struct que implemente “Error()” con el mensaje “Error: el salario
+ingresado no alcanza el mínimo imponible" y lanzalo en caso de que “salary” sea menor a 150.000. De lo contrario,
 tendrás que imprimir por consola el mensaje “Debe pagar impuesto”.
 */
 
@@ -12,24 +12,24 @@ import (
 )
 
 func main() {
-	salary := 100000 
+	salary := 10000
 
-	if salary < 150000 {
-		return 
+	if salary <= 150000 {
+		error := MyError{}
+		fmt.Println(error)
 	} else {
-		fmt.Println("Debe pagar impuesto ")
+		fmt.Println("Debe pagar impuesto")
 	}
 }
 
-type error interface {
-	Error() string
-}
 
-type MyError struct {
-	Message  string
-}
+type MyError struct {}
 
-func (e *MyError) Error() string {
+func (e MyError) Error() string {
 	return "Error: el salario ingresado no alcanza el mínimo imponible"
 }
+
+
+
+
 
