0922f5df29b254438d8fd8086b63e52b93b96f07
laurianderson <laura.anderson@mercadolibre.com>
2023-03-29 19:01:04 -0300

    update: resolve using panic and io, add file customers.txt

--- class4_29march/exercise_7/main.go
+++ class4_29march/exercise_7/main.go
@@ -1,8 +1,8 @@
 /*
-A continuación, vamos a crear un archivo “customers.txt” con información de los clientes del estudio. 
-Ahora que el archivo sí existe, el panic no debe ser lanzado. 
-Creamos el archivo “customers.txt” y le agregamos la información de los clientes. 
-Extendemos el código del punto uno para que podamos leer este archivo e imprimir los datos que contenga. 
+A continuación, vamos a crear un archivo “customers.txt” con información de los clientes del estudio.
+Ahora que el archivo sí existe, el panic no debe ser lanzado.
+Creamos el archivo “customers.txt” y le agregamos la información de los clientes.
+Extendemos el código del punto uno para que podamos leer este archivo e imprimir los datos que contenga.
 En el caso de no poder leerlo, se debe lanzar un “panic”.
 Recordemos que siempre que termina la ejecución, independientemente del resultado, debemos tener un “defer” que
 nos indique que la ejecución finalizó. También recordemos cerrar los archivos al finalizar su uso.
@@ -10,10 +10,36 @@ nos indique que la ejecución finalizó. También recordemos cerrar los archivos
 
 package main
 
-import {
+import (
 	"fmt"
-}
+	"io"
+	"os"
+)
+
 
 func main() {
-	
+	data := readFile("customers.txt")
+
+	fmt.Println(data)
+
 }
+
+func readFile(fileName string) string{
+	f1, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	text, err := io.ReadAll(f1)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f1.Close()
+
+	return string(text)
+}
+
+
+	
+
