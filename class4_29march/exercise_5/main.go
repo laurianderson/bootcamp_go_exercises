89ff65aeedc0f3d326206fd6ce28982cb11b703a
laurianderson <laura.anderson@mercadolibre.com>
2023-03-29 10:57:18 -0300

    feat: add folder exercise_5 with file main.go

---
+++ class4_29march/exercise_5/main.go
@@ -0,0 +1,20 @@
+/*
+Vamos a hacer que nuestro programa sea un poco más complejo y útil. 
+Desarrollá las funciones necesarias para permitir a la empresa calcular:
+Salario mensual de un trabajador según la cantidad de horas trabajadas.
+La función recibirá las horas trabajadas en el mes y el valor de la hora como argumento.
+Dicha función deberá retornar más de un valor (salario calculado y error).
+En caso de que el salario mensual sea igual o superior a $150.000, se le deberá descontar el 10 % en concepto de impuesto.
+En caso de que la cantidad de horas mensuales ingresadas sea menor a 80 o un número negativo, la función debe devolver un error.
+El mismo tendrá que indicar “Error: el trabajador no puede haber trabajado menos de 80 hs mensuales”.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+}
