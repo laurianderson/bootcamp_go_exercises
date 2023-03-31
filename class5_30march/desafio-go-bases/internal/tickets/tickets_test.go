15e5a49270195a7a6772a972df82fd48be410cef
laurianderson <laura.anderson@mercadolibre.com>
2023-03-31 20:45:10 -0300

    update: add testing exercises

--- class5_30march/desafio-go-bases/internal/tickets/tickets_test.go
+++ class5_30march/desafio-go-bases/internal/tickets/tickets_test.go
@@ -1,5 +1,30 @@
 package tickets
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
-func TestGetTotalTickets(t *testing.T) {}
+//Testeo ejercicio 1
+func TestGetTotalTickets(t *testing.T) {
+	ReadDataFile("../../tickets.csv")
+		//Arrange
+		country := "Brazil"
+		expect := 45
+		//Act
+		totalResult,_ := GetTotalTickets(country)
+		//Assertion
+		assert.Equal(t, expect, totalResult)
+}
+
+//Testeo ejercicio 2
+func TestGetTotalMornings(t *testing.T){
+	//Arrange
+	time := "mañana"
+	expect := 256
+	//Act
+	totalResult,_ := GetMornings(time)
+	//Assertion
+	assert.Equal(t, expect, totalResult)
+}
+
